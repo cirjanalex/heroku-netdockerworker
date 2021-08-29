7c9719659b12761b5ddb66750551599c83c3c550
Alexandru Cirjan <cirjan@mds.microsoft.com>
2021-08-29 10:52:21 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace netdockerworker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            while(true) {
+                client.Send(new HttpRequestMessage(
+                    HttpMethod.Post,
+                    "https://stefanini-counter.herokuapp.com/api/counter"
+                ));     
+                Console.WriteLine("Incrementing the counter at https://stefanini-counter.herokuapp.com/");
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
